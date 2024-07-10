d84b7de72fe0e7bc58c27ece1350e06afe83c773
Masrur <mdmasrur3426@gmail.com>
2024-07-10 14:55:26 -0400

    chore: Added pytest

--- app.py
+++ app.py
@@ -11,5 +11,5 @@ def hello_world():
     return 'Hello {}!\n'.format(target)
 
 
-    if __name__ == "__main__":
+if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
